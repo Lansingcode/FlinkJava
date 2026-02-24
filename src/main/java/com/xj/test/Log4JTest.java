a91150558be80e0b06e1d74fdb993b19a1b6509a
xj <jxudut@outlook.com>
2026-02-24 10:16:56 +0800

    添加log4j

---
+++ src/main/java/com/xj/test/Log4JTest.java
@@ -0,0 +1,17 @@
+package com.xj.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Log4JTest {
+    private static final Logger logger = LoggerFactory.getLogger(Log4JTest.class);
+
+    public static void main(String[] args) {
+        // 记录debug级别的信息
+        logger.debug("This is debug message.");
+        // 记录info级别的信息
+        logger.info("This is info message.");
+        // 记录error级别的信息
+        logger.error("This is error message.");
+    }
+}
